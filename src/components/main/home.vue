47f6a760a5c2cb9b2bbefc77b81cd2721edb9791
lenmain <360771751878@qq.com>
2020-09-25 10:58:54 +0800

    确定项目结构

---
+++ src/components/main/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  methods: {
+    handleScroll(e) {
+      e.preventDefault();
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll());
+  }
+};
+</script>
