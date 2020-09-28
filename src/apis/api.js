c00d49f0317e044b2130ae17758e334bfc2adb3c
lenmain <360771751878@qq.com>
2020-09-28 14:18:36 +0800

    初始化api列表

---
+++ src/apis/api.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import Qs from "qs";
+axios.defaults.baseURL = "localhost:8080";
+axios.defaults.withCredentials = true;
+axios.defaults.headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+//声明通过axios与后端通信的api
+
+//-----------------------------------------
+//用户信息以及登陆注册相关api
+export const logIn = (userName, password) => {
+  return axios.post("/user/login", Qs.stringify({ userName, password }));
+};
+
+export const signIn = (userName, password) => {
+  return axios.post("/user/signin", Qs.stringify({ userName, password }));
+};
+
+//-----------------------------------------
+
+//-----------------------------------------
+//计时相关操作
+export const getTime = () => {
+  return axios.post("/timing/gettime");
+};
+export const startTiming = () => {
+  return axios.post("/timing/start");
+};
+export const stopTiming = () => {
+  return axios.post("/timing/stop");
+};
+//-----------------------------------------
+
+//-----------------------------------------
+//计划相关api
+export const deletePlan = () => {
+  return axios.post("/plan/delete");
+};
+
+export const newPlan = (days, duration) => {
+  return axios.post("/plan/new".Qs.stringify({ days, duration }));
+};
+//-----------------------------------------
--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
+import moment from "moment"; //导入文件
+import vuetify from "./plugins/vuetify";
+Vue.use(vuetify);
+Vue.prototype.$moment = moment; //赋值使用
+moment.locale("zh-cn"); //国际化，中文
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
